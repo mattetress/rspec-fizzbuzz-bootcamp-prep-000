b8e06dc35681db39af6ddb266d0d3be6bad3673f
Matthew Etress <mattetress@me.com>
2018-09-06 21:26:13 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,13 +3,10 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
     return "FizzBuzz"
-  end
   elsif int % 3 == 0
     return "Fizz"
-  end
   elsif int % 5 == 0
     return "Buzz"
-  end
   else
     return nil
   end
